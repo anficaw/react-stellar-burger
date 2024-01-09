52592f28d08e1a75bcb69c7e40aee892197b9fd6
Olga Vertiankina <anficaw@mail.ru>
2024-01-09 14:03:58 +0000

    newr

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,41 +1,265 @@
-import React from "react";
-import styles from './burger-constructor.module.css';
-import { ConstructorElement } from "@ya.praktikum/react-developer-burger-ui-components";
+import React, { useState } from "react";
+import styles from "./burger-constructor.module.css";
+import {
+  ConstructorElement,
+  DragIcon,
+  Button,
+  CurrencyIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import Icon from "../../images/icon 36x36.svg";
+import Modal from "../modal/modal";
+import OrderDetails from "../order-details/order-details";
 
+function BurgerConstructor() {
+  const [isModalOpen, setModalOpen] = useState(false);
 
+  const onClose = () => {
+    setModalOpen(false);
+  };
 
-function BurgerConstructor(cards) {
-    const datan = cards.cards.cards.slice(0);
+  const onOpen = () => {
+    setModalOpen(true);
+  };
 
+  let number = 1;
 
-    const NewBurger = {
-        bun: datan[0],
-        ingradients: datan.slice(1,5)
-    }
+  const NewBurger = {
+    bun: {
+      _id: "643d69a5c3f7b9001cfa093c",
+      name: "Краторная булка N-200i",
+      type: "bun",
+      proteins: 80,
+      fat: 24,
+      carbohydrates: 53,
+      calories: 420,
+      price: 1255,
+      image: "https://code.s3.yandex.net/react/code/bun-02.png",
+      image_mobile: "https://code.s3.yandex.net/react/code/bun-02-mobile.png",
+      image_large: "https://code.s3.yandex.net/react/code/bun-02-large.png",
+      __v: 0,
+    },
+    ingradients: [
+      {
+        _id: "643d69a5c3f7b9001cfa0941",
+        name: "Биокотлета из марсианской Магнолии",
+        type: "main",
+        proteins: 420,
+        fat: 142,
+        carbohydrates: 242,
+        calories: 4242,
+        price: 424,
+        image: "https://code.s3.yandex.net/react/code/meat-01.png",
+        image_mobile:
+          "https://code.s3.yandex.net/react/code/meat-01-mobile.png",
+        image_large: "https://code.s3.yandex.net/react/code/meat-01-large.png",
+        __v: 0,
+      },
+      {
+        _id: "643d69a5c3f7b9001cfa093e",
+        name: "Филе Люминесцентного тетраодонтимформа",
+        type: "main",
+        proteins: 44,
+        fat: 26,
+        carbohydrates: 85,
+        calories: 643,
+        price: 988,
+        image: "https://code.s3.yandex.net/react/code/meat-03.png",
+        image_mobile:
+          "https://code.s3.yandex.net/react/code/meat-03-mobile.png",
+        image_large: "https://code.s3.yandex.net/react/code/meat-03-large.png",
+        __v: 0,
+      },
+      {
+        _id: "643d69a5c3f7b9001cfa093f",
+        name: "Мясо бессмертных моллюсков Protostomia",
+        type: "main",
+        proteins: 433,
+        fat: 244,
+        carbohydrates: 33,
+        calories: 420,
+        price: 1337,
+        image: "https://code.s3.yandex.net/react/code/meat-02.png",
+        image_mobile:
+          "https://code.s3.yandex.net/react/code/meat-02-mobile.png",
+        image_large: "https://code.s3.yandex.net/react/code/meat-02-large.png",
+        __v: 0,
+      },
+      {
+        _id: "643d69a5c3f7b9001cfa0940",
+        name: "Говяжий метеорит (отбивная)",
+        type: "main",
+        proteins: 800,
+        fat: 800,
+        carbohydrates: 300,
+        calories: 2674,
+        price: 3000,
+        image: "https://code.s3.yandex.net/react/code/meat-04.png",
+        image_mobile:
+          "https://code.s3.yandex.net/react/code/meat-04-mobile.png",
+        image_large: "https://code.s3.yandex.net/react/code/meat-04-large.png",
+        __v: 0,
+      },
+      {
+        _id: "643d69a5c3f7b9001cfa093d",
+        name: "Флюоресцентная булка R2-D3",
+        type: "bun",
+        proteins: 44,
+        fat: 26,
+        carbohydrates: 85,
+        calories: 643,
+        price: 988,
+        image: "https://code.s3.yandex.net/react/code/bun-01.png",
+        image_mobile: "https://code.s3.yandex.net/react/code/bun-01-mobile.png",
+        image_large: "https://code.s3.yandex.net/react/code/bun-01-large.png",
+        __v: 0,
+      },
+      {
+        _id: "643d69a5c3f7b9001cfa0944",
+        name: "Соус традиционный галактический",
+        type: "sauce",
+        proteins: 42,
+        fat: 24,
+        carbohydrates: 42,
+        calories: 99,
+        price: 15,
+        image: "https://code.s3.yandex.net/react/code/sauce-03.png",
+        image_mobile:
+          "https://code.s3.yandex.net/react/code/sauce-03-mobile.png",
+        image_large: "https://code.s3.yandex.net/react/code/sauce-03-large.png",
+        __v: 0,
+      },
+      {
+        _id: "643d69a5c3f7b9001cfa0945",
+        name: "Соус с шипами Антарианского плоскоходца",
+        type: "sauce",
+        proteins: 101,
+        fat: 99,
+        carbohydrates: 100,
+        calories: 100,
+        price: 88,
+        image: "https://code.s3.yandex.net/react/code/sauce-01.png",
+        image_mobile:
+          "https://code.s3.yandex.net/react/code/sauce-01-mobile.png",
+        image_large: "https://code.s3.yandex.net/react/code/sauce-01-large.png",
+        __v: 0,
+      },
+      {
+        _id: "643d69a5c3f7b9001cfa0946",
+        name: "Хрустящие минеральные кольца",
+        type: "main",
+        proteins: 808,
+        fat: 689,
+        carbohydrates: 609,
+        calories: 986,
+        price: 300,
+        image: "https://code.s3.yandex.net/react/code/mineral_rings.png",
+        image_mobile:
+          "https://code.s3.yandex.net/react/code/mineral_rings-mobile.png",
+        image_large:
+          "https://code.s3.yandex.net/react/code/mineral_rings-large.png",
+        __v: 0,
+      },
+      {
+        _id: "643d69a5c3f7b9001cfa0947",
+        name: "Плоды Фалленианского дерева",
+        type: "main",
+        proteins: 20,
+        fat: 5,
+        carbohydrates: 55,
+        calories: 77,
+        price: 874,
+        image: "https://code.s3.yandex.net/react/code/sp_1.png",
+        image_mobile: "https://code.s3.yandex.net/react/code/sp_1-mobile.png",
+        image_large: "https://code.s3.yandex.net/react/code/sp_1-large.png",
+        __v: 0,
+      },
+      {
+        _id: "643d69a5c3f7b9001cfa0948",
+        name: "Кристаллы марсианских альфа-сахаридов",
+        type: "main",
+        proteins: 234,
+        fat: 432,
+        carbohydrates: 111,
+        calories: 189,
+        price: 762,
+        image: "https://code.s3.yandex.net/react/code/core.png",
+        image_mobile: "https://code.s3.yandex.net/react/code/core-mobile.png",
+        image_large: "https://code.s3.yandex.net/react/code/core-large.png",
+        __v: 0,
+      },
+      {
+        _id: "643d69a5c3f7b9001cfa0949",
+        name: "Мини-салат Экзо-Плантаго",
+        type: "main",
+        proteins: 1,
+        fat: 2,
+        carbohydrates: 3,
+        calories: 6,
+        price: 4400,
+        image: "https://code.s3.yandex.net/react/code/salad.png",
+        image_mobile: "https://code.s3.yandex.net/react/code/salad-mobile.png",
+        image_large: "https://code.s3.yandex.net/react/code/salad-large.png",
+        __v: 0,
+      },
+    ],
+  };
+  let summ = NewBurger.bun.price;
+  {
+    NewBurger.ingradients.map((item) => {
+      summ = summ + item.price;
+    });
+  }
 
-     console.log (NewBurger);
+  return (
+    <section className={styles.burgerconstructor}>
+      <div className="mt-25 pt-4 pl-8 mr-7">
+        <ConstructorElement
+          type="top"
+          isLocked={true}
+          text={NewBurger.bun.name}
+          price={NewBurger.bun.price}
+          thumbnail={NewBurger.bun.image_mobile}
+          key={NewBurger.bun._id}
+        />
+      </div>
+      <div className={`custom-scroll ${styles.burgerConstructorList}`}>
+        {NewBurger.ingradients.map((item) => {
+          return (
+            <div className="mt-4" key={item._id}>
+              <DragIcon className="mr-2" />
+              <ConstructorElement
+                text={item.name}
+                price={item.price}
+                thumbnail={item.image_mobile}
+              />
+            </div>
+          );
+        })}
+      </div>
 
-    return(
-        <section className={styles.burgerconstructor}>
-            <ConstructorElement
-              type="top"
-              isLocked={true}
-              text = {NewBurger.bun.name}
-              price= {NewBurger.bun.price}
-              thumbnail={NewBurger.bun.image_mobile}
-             />
-            
-             
-            <ConstructorElement
-              type="bottom"
-              isLocked={true}
-              text = {NewBurger.bun.name}
-              price= {NewBurger.bun.price}
-              thumbnail={NewBurger.bun.image_mobile}
-             />
-
-        </section>
-
-    )
-};
+      <div className="mb-4 mt-4 pl-8 mr-7">
+        <ConstructorElement
+          type="bottom"
+          isLocked={true}
+          text={NewBurger.bun.name}
+          price={NewBurger.bun.price}
+          thumbnail={NewBurger.bun.image_mobile}
+          key={NewBurger.bun._id}
+        />
+      </div>
+      <div className={`mt-10 text text_type_digits-medium ${styles.summa}`}>
+        <div>{summ}</div>
+        <img className={styles.image} src={Icon} alt="бриллиант"></img>
+        <Button htmlType="button" type="primary" size="large" onClick={onOpen}>
+          Оформить заказ
+        </Button>
+      </div>
+      {isModalOpen && (
+        <Modal onClose={onClose}>
+          <OrderDetails number={number} />
+        </Modal>
+      )}
+    </section>
+  );
+}
 export default BurgerConstructor;
