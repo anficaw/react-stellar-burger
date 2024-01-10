4878de3f057c08f8e7f3ce2a4d887ca8193062ce
Olga Vertiankina <anficaw@mail.ru>
2024-01-10 13:34:20 +0000

    newr

--- src/components/burger-ingredient/burger-ingredient.jsx
+++ src/components/burger-ingredient/burger-ingredient.jsx
@@ -1,24 +1,36 @@
+import React, { useState } from "react";
+import styles from "./burger-ingredient.module.css";
+import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import Modal from "../modal/modal";
+import IngredientDetails from "../ingredient-details/ingredient-details";
 
- import React from "react";
- import styles from './burger-ingredient.module.css';
- import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
- 
- 
- function BurgerIngredient(card) {
-     
-     let number=1;
-     return(
-          
-         <div className={styles.burgerIngredient}>
-            <img className={styles.image} alt={card.card.name} src={card.card.image}/>
-            <div className={styles.imageprice}>
-                    <div className=" mr-2 text text_type_digits-default">{card.card.price}</div>
-                    <CurrencyIcon/>
-            </div>
-            <div className={`text text_type_main-default ${styles.title}`}>{card.card.name}</div>
-            <div className={`text text_type_digits-default ${styles.nom}`}>{card.card.num}</div>
-         </div>
- 
-     )
- };
- export default BurgerIngredient;
+function BurgerIngredient({ card, detalesingradient }) {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const onClose = () => {
+    setModalOpen(false);
+  };
+
+  const onOpen = () => {
+    setModalOpen(true);
+  };
+  let number = 1;
+  return (
+    <div
+      className={styles.burgerIngredient}
+      onClick={() => detalesingradient(card)}
+    >
+      <img className={styles.image} alt={card.name} src={card.image} />
+      <div className={styles.imageprice}>
+        <div className=" mr-2 text text_type_digits-default">{card.price}</div>
+        <CurrencyIcon />
+      </div>
+      <div className={`text text_type_main-default ${styles.title}`}>
+        {card.name}
+      </div>
+      <div className={`text text_type_digits-default ${styles.nom}`}>
+        {card.num}
+      </div>
+    </div>
+  );
+}
+export default BurgerIngredient;
